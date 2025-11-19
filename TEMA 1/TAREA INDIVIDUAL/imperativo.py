ad865757bf289f91e954585a23c53edd42033fec
Arianna <ariannavilchez2@gmail.com>
2025-11-19 16:07:26 +0100

    Tarea Individual - Ejercicios de Python organizados

---
+++ TEMA 1/TAREA INDIVIDUAL/imperativo.py
@@ -0,0 +1,8 @@
+longitud = 10
+ancho = 5
+
+area = longitud * ancho
+
+print(f"La longitud del rectángulo es: {longitud}")
+print(f"El ancho del rectángulo es: {ancho}")
+print(f"El área del rectángulo es: {area}")
---
+++ TEMA 1/TAREA INDIVIDUAL/ra1_c.py
@@ -0,0 +1,5 @@
+nombre = input("Ingrese su nombre: ")
+
+print(f"Hola, {nombre}. Bienvenido/a al programa.")
+
+print(f"Tu nombre tiene un total de: {len(nombre)} caracteres.")
